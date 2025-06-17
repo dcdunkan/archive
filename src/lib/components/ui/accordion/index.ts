3707b772548ede960861d1d5d4fb24d45460dfdb
Dunkan <70066170+dcdunkan@users.noreply.github.com>
2025-06-18 04:05:51 +0530

    include more components
    focusing on the actual changes in the next commit

---
+++ src/lib/components/ui/accordion/index.ts
@@ -0,0 +1,16 @@
+import Content from "./accordion-content.svelte";
+import Item from "./accordion-item.svelte";
+import Trigger from "./accordion-trigger.svelte";
+import Root from "./accordion.svelte";
+
+export {
+	Content,
+	Content as AccordionContent,
+	Item,
+	Item as AccordionItem,
+	Root,
+	//
+	Root as Accordion,
+	Trigger,
+	Trigger as AccordionTrigger,
+};
